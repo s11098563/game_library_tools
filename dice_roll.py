606c302acada8746d83627fc2e0724a78ccf7e68
Connor <s11098563@students.duvalschools.org>
2021-02-18 14:01:44 -0500

    [8B] v0.2
    Defined roll_d4
    Defined variables rolls and sum.
    Added while loop to roll until num_rolls is met.

--- dice_roll.py
+++ dice_roll.py
@@ -1,4 +1,12 @@
-# Dice Rolling Library, Connor Cook, Feb 18, 2021, 1:44PM v0.0
+# Dice Rolling Library, Connor Cook, Feb 18, 2021, 1:59PM v0.2
 
 import random
 
+# d4 simulator
+def roll_d4(num_roll): # num_roll is an arguement
+    rolls = 0
+    sum = 0
+
+    while rolls < num_roll:
+        result = random.randint(1, 4)
+        print(f"You have rolled a {result}.\n")
