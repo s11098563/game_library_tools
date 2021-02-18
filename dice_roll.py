2ac6768d542c43ef0a6765070b5022d9f86f6bfd
Connor <s11098563@students.duvalschools.org>
2021-02-18 14:05:38 -0500

    [8B] v0.3
    Added 1 to rolls in while loop.
    Added result to the sum.
    Printed total sum of all rolls.

--- dice_roll.py
+++ dice_roll.py
@@ -1,4 +1,4 @@
-# Dice Rolling Library, Connor Cook, Feb 18, 2021, 1:59PM v0.2
+# Dice Rolling Library, Connor Cook, Feb 18, 2021, 2:04PM v0.3
 
 import random
 
@@ -10,3 +10,10 @@ def roll_d4(num_roll): # num_roll is an arguement
     while rolls < num_roll:
         result = random.randint(1, 4)
         print(f"You have rolled a {result}.\n")
+        rolls += 1
+        sum += result
+    
+    print(f"The sum of all your rolls is {sum}.\n")
+
+
+roll_d4()
